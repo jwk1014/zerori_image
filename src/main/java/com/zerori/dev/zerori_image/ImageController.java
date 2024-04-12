4f7bdeb8dd9a5796d48bd880d6b3ab55c89e067b
jwk1014 <admin@devwon.com>
2024-04-12 20:24:41 +0900

    fix: @RequestMapping("/api") 추가

--- src/main/java/com/zerori/dev/zerori_image/ImageController.java
+++ src/main/java/com/zerori/dev/zerori_image/ImageController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
+@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class ImageController {
